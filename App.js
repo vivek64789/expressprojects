d4ea393f1bdb79ccd6c644c495eb43fb5d56cc34
Bibekanand Kushwaha <vivek@Bibekanands-MacBook-Pro.local>
2021-12-12 15:49:50 +0545

    authentication using express

---
+++ App.js
@@ -0,0 +1,22 @@
+
+const { appendFile } = require('fs');
+const mongoos = require('mongoose');
+
+
+const express =require('express');
+
+const app= express();
+
+require("./src/database/DatabaseConnection");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const customerRoute = require("./src/routers/CustomerRouter");
+app.use(customerRoute);
+
+app.listen(3000, () => {
+    console.log('server listening ata port 3000');
+});
+
+
+
---
+++ src/auth/Auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.verifyToken = (req, res, next) => {
+    // const bearerHeader = req.headers['authorization'];
+    // if (typeof bearerHeader !== 'undefined') {
+    //     const bearer = bearerHeader.split(' ');
+    //     const bearerToken = bearer[1];
+    //     req.token = bearerToken;
+    //     next();
+    // } else {
+    //     res.sendStatus(403);
+    // }
+
+    const token = req.headers.authorization;
+    console.log(token);
+}
